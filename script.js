325f4f68a348232b2d4dcad6ff0a38533ce52540
pico0513 <6myjk9yycv@privaterelay.appleid.com>
2026-09-22 15:44:18 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-a
+const choiceA = document.getElementById("choiceA");
+const choiceB = document.getElementById("choiceB");
+const message = document.getElementById("message");
+const round = document.getElementById("round");
+
+let currentRound = 1;
+
+function choose(choice) {
+
+  message.textContent = `「${choice}」を選びました！`;
+
+  currentRound++;
+
+  setTimeout(() => {
+    round.textContent = `ROUND ${currentRound}`;
+    message.textContent = "";
+  }, 700);
+}
+
+choiceA.addEventListener("click", () => {
+  choose("自撮り A");
+});
+
+choiceB.addEventListener("click", () => {
+  choose("自撮り B");
+});
