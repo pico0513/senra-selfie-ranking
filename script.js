a1750c6e97d9670c3fc10e374ef5f8602838b797
pico0513 <6myjk9yycv@privaterelay.appleid.com>
2026-09-22 15:53:31 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -5,6 +5,29 @@ const round = document.getElementById("round");
 
 let currentRound = 1;
 
+// テスト用のX投稿URL
+const posts = {
+  A: "https://x.com/X/status/123456789",
+  B: "https://x.com/X/status/987654321"
+};
+
+function showPost(button, url) {
+  const box = button.querySelector(".post-box");
+
+  box.innerHTML = `
+    <blockquote class="twitter-tweet">
+      <a href="${url}"></a>
+    </blockquote>
+  `;
+
+  if (window.twttr && window.twttr.widgets) {
+    window.twttr.widgets.load(box);
+  }
+}
+
+showPost(choiceA, posts.A);
+showPost(choiceB, posts.B);
+
 function choose(choice) {
 
   message.textContent = `「${choice}」を選びました！`;
@@ -18,9 +41,9 @@ function choose(choice) {
 }
 
 choiceA.addEventListener("click", () => {
-  choose("自撮り A");
+  choose("左の投稿");
 });
 
 choiceB.addEventListener("click", () => {
-  choose("自撮り B");
+  choose("右の投稿");
 });
